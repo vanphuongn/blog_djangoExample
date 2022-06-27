65c4558c70edfb2c478038d001c18b51e6100423
Phuong <nvphuongdt05@gmail.com>
2022-06-28 00:47:03 +0700

    xx

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,78 +1,38 @@
 from django.db import models
-from django.utils import  timezone
+from django.utils import timezone
 from django.contrib.auth.models import User
-from django.urls import  reverse
+# Create your models here.
+
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager,
-                     self).get_queryset() \
-            .filter(status='published')
-
-# Create your models here.
+        return super(PublishedManager,self).get_queryset().filter(status='published')
 class Post(models.Model):
     STATUS_CHOICES = (
-    ('draft', 'Draft'),
-    ('published', 'Published'),
+        ('draft', 'Draft'),
+        ('published', 'Published'),
     )
-
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
     author = models.ForeignKey(User,
-        on_delete=models.CASCADE,
-        related_name='blog_posts')
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
     body = models.TextField()
-
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,
-
-    choices=STATUS_CHOICES,
-    default='draft')
-
-    #cac lop co the chua cac metadata(sieu du lieu) luu thong tin ve cac thiet lap cua model
-    # cac thong tin nay ko dc dung de tao bang
-    # luu tru thong tin cau hinh cua model, vi du nhu orderting: tuc sap xep du lieu dc tra ve mac dinh theo cot nao
-    #vd:"" db_table = "oxen" thi
-    class Meta:
-        ordering = ('-publish',)
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse('blog:post_detail',
-                       args=[self.publish.year,
-                             self.publish.month,
-                             self.publish.day, self.slug])
+                              choices=STATUS_CHOICES,
+                              default='draft')
 
     objects = models.Manager()
     published = PublishedManager()
 
+    class Meta:
 
-class Blog(models.Model):
-    name = models.CharField(max_length=100)
-    tagline = models.TextField()
-    def __str__(self):
-        return self.name
+        ordering = ('-publish',)
 
-class Author(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField()
 
     def __str__(self):
-        return self.name
-
-class Entry(models.Model):
-    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
-    headline = models.CharField(max_length=255)
-    body_text = models.TextField()
-    pub_date = models.DateField()
-    mod_date = models.DateField()
-    authors = models.ManyToManyField(Author)
-    n_comments = models.IntegerField()
-    n_pingbacks = models.IntegerField()
-    rating = models.IntegerField()
-
-    def __str__(self):
-        return self.headline
+        return self.title
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,11 +1,8 @@
-from django.urls import  path
+from django.urls import path
 from . import views
 
 app_name = "blog"
-
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-    views.post_detail,
-    name='post_detail'),
+    path('', views.post_list,name="list"),
+    path('<int:post_id>/', views.post_detail, name="detail"),
 ]
